1a796d878d93ed32fb60c3c0d3bb26fa1a37b801
raul <raul@zamacona.me>
2013-09-15 15:06:46 -0500

    cambio de color de buscador

--- themes/alias/js/functions.js
+++ themes/alias/js/functions.js
@@ -14,9 +14,39 @@
 			}
 		},100);
 
+		//Buscador
+		$('#form_buscador').on('click', function(){
+			$(this).css({
+				'background-color': 'black'
+			});
+			$(this).find('input').css({
+				'background-color': 'black',
+				'color': 'white'
+			});
+			$(this).find('#submit').css({
+				'background-position': '0px -13px'
+			});
+		});
+
+		$('body').on('click', function(e){
+
+			if ( ! $(e.target).hasClass('s') ){
+				var form_buscador = $('#form_buscador');
+				$(form_buscador).css({
+					'background-color': 'white'
+				});
+				$(form_buscador).find('input').css({
+					'background-color': 'white',
+					'color': 'black'
+				});
+				$(form_buscador).find('#submit').css({
+					'background-position': '0px 0px'
+				});
 
 
+			}
 
+		});
 
 	});
 
--- themes/alias/searchform.php
+++ themes/alias/searchform.php
@@ -1,5 +1,5 @@
 <form id="form_buscador" class="form_buscador borde_gris sidebar_elemento" action="<?php bloginfo('siteurl'); ?>"  method="get">
-	<input type="submit" value="">
-	<input name="s" type="text" id="s">
+	<input type="submit" value="" id="submit">
+	<input name="s" type="text" id="s" class="s">
 </form>
 
