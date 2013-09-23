0321fb5f5d9c6aefe79e41de30f6f29196a963f6
scrub <scrub.mx@gmail.com>
2013-09-23 03:23:27 -0500

    error fix

--- plugins/tangentlabs-newsletter/Newsletter.class.php
+++ plugins/tangentlabs-newsletter/Newsletter.class.php
@@ -87,7 +87,7 @@
 				<div id="newsletter-content">
 					<form method="POST" action="">
 						<label for="title" class="label-title">Título</label>
-						<input type='text' class='widefat' id="title" name='newsletter_title' value='' placeholder="email title">
+						<input type='text' class='widefat' id="title" name='newsletter_title' value=''>
 
 						<label for="newsletter_content" class="label-title">Contenido</label>
 						<?php wp_editor( '','newsletter_content', array('textarea_name' => 'newsletter_content','media_buttons' => false) ); ?>
