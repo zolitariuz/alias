cfc55b985d0f51f3e9718fea6ea4b27cc0c4ecf7
scrub <scrub.mx@gmail.com>
2013-10-09 11:47:50 -0500

    quitar punto del aviso de privacidad

--- themes/alias/footer.php
+++ themes/alias/footer.php
@@ -4,7 +4,11 @@
 
 			<div class="footer">
 
-				<p class="aviso"><a href="<?php echo qtrans_convertURL(site_url('/aviso-de-privacidad/')) ?>"><?php _e('Aviso de privacidad', 'alias') ?>.</a></p>
+				<p class="aviso">
+					<a href="<?php echo qtrans_convertURL(site_url('/aviso-de-privacidad/')) ?>">
+						<?php _e('Aviso de privacidad', 'alias') ?>
+					</a>
+				</p>
 
 				<?php get_template_part( 'templates/footer', 'librerias' ) ?>
 
