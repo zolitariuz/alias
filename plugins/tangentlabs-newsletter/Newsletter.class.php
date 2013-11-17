1e71ce1ea3a9a53a7fe65ef6de3efafa0164fdee
scrub <scrub.mx@gmail.com>
2013-11-17 13:10:28 -0600

    mail nativo

--- plugins/tangentlabs-newsletter/Newsletter.class.php
+++ plugins/tangentlabs-newsletter/Newsletter.class.php
@@ -127,12 +127,47 @@
 
 			//Newsletter::send_multiple_recipients($recipients, $subject, $message);
 			//$message = '<div style="width: 600px; margin: 0 auto" ><div style="width:300px; height: 200px; background: #00ff00"></div><div style="width:300px; height: 200px; background: #0000ff""></div>';
+			//add_filter( 'wp_mail_content_type', array('Newsletter', 'set_html_content_type') );
+			//wp_mail('scrub.mx@gmail.com', $subject, $message, 'From: Alias <informes@aliaseditorial.com>');
+			//remove_filter( 'wp_mail_content_type', array('Newsletter', 'set_html_content_type') );
 
-			add_filter( 'wp_mail_content_type', array('Newsletter', 'set_html_content_type') );
 
-			wp_mail('scrub.mx@gmail.com', $subject, $message, 'From: Alias <informes@aliaseditorial.com>');
+			//define the receiver of the email
+			$to = 'scrub.mx@gmail.com';
+			//define the subject of the email
+			$subject = 'Test HTML email';
+			//create a boundary string. It must be unique
+			//so we use the MD5 algorithm to generate a random hash
+			$random_hash = md5(date('r', time()));
+			//define the headers we want passed. Note that they are separated with \r\n
+			$headers = "From: webmaster@example.com\r\nReply-To: webmaster@example.com";
+			//add boundary string and mime type specification
+			$headers .= "\r\nContent-Type: multipart/alternative; boundary=\"PHP-alt-".$random_hash."\"";
+			//define the body of the message.
+			ob_start(); //Turn on output buffering
+			?>
+			--PHP-alt-<?php echo $random_hash; ?>
+			Content-Type: text/plain; charset="iso-8859-1"
+			Content-Transfer-Encoding: 7bit
 
-			remove_filter( 'wp_mail_content_type', array('Newsletter', 'set_html_content_type') );
+			Hello World!!!
+			This is simple text email message.
+
+			--PHP-alt-<?php echo $random_hash; ?>
+			Content-Type: text/html; charset="iso-8859-1"
+			Content-Transfer-Encoding: 7bit
+
+			<h2>Hello World!</h2>
+			<p>This is something with <b>HTML</b> formatting.</p>
+
+			--PHP-alt-<?php echo $random_hash; ?>--
+			<?php
+			//copy current buffer contents into $message variable and delete current output buffer
+			$message = ob_get_clean();
+			//send the email
+			$mail_sent = @mail( $to, $subject, $message, $headers );
+			//if the message is sent successfully print "Mail sent". Otherwise print "Mail failed"
+			echo $mail_sent ? "Mail sent" : "Mail failed";
 		}
 
 
