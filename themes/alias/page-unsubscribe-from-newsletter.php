60c9949fe687e71d85739cc30eda1dd0ea563ed0
scrub <scrub.mx@gmail.com>
2013-12-09 01:55:06 -0600

    mas pruebas mail...

--- themes/alias/page-unsubscribe-from-newsletter.php
+++ themes/alias/page-unsubscribe-from-newsletter.php
@@ -2,5 +2,5 @@
 
 	if isset($_GET['email']){
 		global $wpdb;
-		$wpdb->query( "DELETE FROM {$wpdb->prefix}newsletter WHERE email = $email" );
+		$wpdb->query( "DELETE FROM {$wpdb->prefix}newsletter WHERE email = '$email'" );
 	}
