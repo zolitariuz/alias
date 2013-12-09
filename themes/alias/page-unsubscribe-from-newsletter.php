202fbb940a418a28a667094a3c4af0fad77cdc10
scrub <scrub.mx@gmail.com>
2013-12-09 01:57:44 -0600

    mas pruebas mail......

--- themes/alias/page-unsubscribe-from-newsletter.php
+++ themes/alias/page-unsubscribe-from-newsletter.php
@@ -2,5 +2,8 @@
 
 	if isset($_GET['email']){
 		global $wpdb;
+		$email = $_GET['email'];
 		$wpdb->query( "DELETE FROM {$wpdb->prefix}newsletter WHERE email = '$email'" );
+	}else {
+		wp_redirect( site_url() );
 	}
