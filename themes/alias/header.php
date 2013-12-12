6946159e01cfcdab7f585428699f2c3c88cf84b2
raul <raul@zamacona.me>
2013-12-11 20:10:11 -0600

    movil

--- themes/alias/header.php
+++ themes/alias/header.php
@@ -4,6 +4,8 @@
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="description" content="<?php bloginfo('description') ?>">
+		 <meta name="HandheldFriendly" content="True">
+        <meta name="MobileOptimized" content="320">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title><?php print_title(); ?></title>
 		<?php wp_head() ?>
