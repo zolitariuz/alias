b8363f5a8aae79cdb0ee64e91930bdd29c82d2e9
scrub <scrub.mx@gmail.com>
2014-01-15 11:15:55 -0600

    prueba stripslashes MAS

--- plugins/tangentlabs-newsletter/Newsletter.class.php
+++ plugins/tangentlabs-newsletter/Newsletter.class.php
@@ -125,13 +125,17 @@
 		public static function send($subject, $message)
 		{
 
-			// $headers  = "From: Alias <informes@aliaseditorial.com>\r\nReply-To: informes@aliaseditorial.com";
-			// $headers .= "\r\nContent-Type: multipart/alternative;";
+			$headers  = "From: Alias <informes@aliaseditorial.com>\r\nReply-To: informes@aliaseditorial.com";
+			$headers .= "\r\nContent-Type: multipart/alternative;";
+
+			add_filter( 'wp_mail_content_type', array('Newsletter', 'set_html_content_type') );
 
 			$message = stripslashes($message);
 
 			wp_mail('scrub.mx@gmail.com', 'prueba stripslashes 2 DUO', $message);
 			wp_mail('raul.zdesign@gmail.com', 'prueba stripslashes 2 DUO', $message);
+
+			remove_filter( 'wp_mail_content_type', array('Newsletter', 'set_html_content_type') );
 
 			// $recipients  = Newsletter::get_mails();
 			// if ( ! $recipients) return false;
