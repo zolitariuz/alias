5a675f3899f8f5f09bcb57ceace92a0fee76c679
scrub <scrub.mx@gmail.com>
2014-01-15 09:18:38 -0600

    pruebas mail

--- plugins/tangentlabs-newsletter/Newsletter.class.php
+++ plugins/tangentlabs-newsletter/Newsletter.class.php
@@ -133,7 +133,10 @@
 			$headers  = "From: Alias <informes@aliaseditorial.com>\r\nReply-To: informes@aliaseditorial.com";
 			$headers .= "\r\nContent-Type: multipart/alternative; boundary=\"PHP-alt-".$random_hash."\"";
 
-			Newsletter::send_multiple_recipients($recipients, $subject, $message, $headers);
+			// Newsletter::send_multiple_recipients($recipients, $subject, $message, $headers);
+
+			wp_mail('raul.zdesign@gmail.com', $subject, stripslashes($message), $headers);
+
 			//add_filter( 'wp_mail_content_type', array('Newsletter', 'set_html_content_type') );
 			//wp_mail($recipients, $subject, stripslashes($message), $headers);
 			//remove_filter( 'wp_mail_content_type', array('Newsletter', 'set_html_content_type') );
